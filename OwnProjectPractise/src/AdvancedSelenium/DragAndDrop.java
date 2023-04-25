85f108294c49b3afffc22b2b2cf25eb84f7e8906
Sarathi <Sarathik86@gmail.com>
2023-04-25 23:22:03 +0530

    Examples for the Drag and drop

---
+++ OwnProjectPractise/src/AdvancedSelenium/DragAndDrop.java
@@ -0,0 +1,33 @@
+package AdvancedSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDrop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String url = "https://www.leafground.com/drag.xhtml";
+		String driverPath = "C:\\Users\\HP\\Desktop\\Sarathi Drive\\Automation Projects\\drivers\\chromedriver.exe";
+		
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		
+		WebElement dragElement = driver.findElement(By.xpath("//*[@id=\"form:drag_content\"]"));
+		WebElement toElement = driver.findElement(By.xpath("//*[@id=\"form:drop_header\"]"));
+		
+		Actions act = new Actions(driver);
+		act.dragAndDrop(dragElement,toElement );
+		act.build().perform();
+		
+		driver.close();
+		
+
+	}
+
+}
