2fcd374b73d95f4303a9f9c833733114f51b0807
Sarathi <Sarathik86@gmail.com>
2023-06-24 18:24:31 -0500

    Hierarichal Inheritance

---
+++ OwnProjectPractise/src/javaPractise/VMobileType2.java
@@ -0,0 +1,15 @@
+package javaPractise;
+
+public class VMobileType2 extends VivoPhones{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		VMobileType2 vtm2 = new VMobileType2();
+		
+		vtm2.vivoMethod();
+		vtm2.smMethod();
+
+	}
+
+}
