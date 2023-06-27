0b670ab5776755909e9ff2017881f1cb3a070bb8
Sarathi <Sarathik86@gmail.com>
2023-06-26 22:08:05 -0500

    ..

---
+++ OwnProjectPractise/src/TestNgPractice/testNGDemo.java
@@ -0,0 +1,13 @@
+package TestNgPractice;
+
+public class testNGDemo {
+	
+	public void testCase1() {
+		System.out.println("This is the Test Case 1 - Confirmation");
+	}
+	
+	public void testCase2() {
+		System.out.println("This is the Test Case 2 - Confirmation");
+	}
+
+}
